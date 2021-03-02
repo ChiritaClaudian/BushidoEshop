ee94efbe39121236ec3aa4fb07479847dccd8f80
Chirita Claudian <chiritaclaudian@gmail.com>
2021-03-02 19:12:03 +0200

    Features of main page done

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -3,12 +3,10 @@ import "bootstrap"
 import 'react-bootstrap'
 import backgroundVideo from '../resources/martialArts.mp4'
 import classes from '../modules/BackgroundVideo.module.css'
-import { Container, Row, Col, Button, Jumbotron } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import '../resources/RobotoFont/Roboto-Medium.ttf'
 import {useMediaQuery} from 'react-responsive'
-import mainpageButtons from '../components/mainPageButtons'
 import backgroundImage from '../resources/bushido_cover.jpg'
-
 
 function BackgroundVideo(props)
 {
@@ -48,27 +46,26 @@ function BackgroundVideo(props)
         return(
             <div className={classes.container} > 
                 <Container fluid="md" className=" align-self-center justify-content-md-center ">
-            <Row className="py-auto justify-content-md-center ">
-                <Col md="auto" >
-                    <p className={classes.content}>
-                        Wellcome to the Busho E-shop 
-                    </p>
-                </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-                <Col className={classes.buttons}>
-                    <Button variant="light">
-                        Shop Now
-                    </Button>{' '}
-                    <Button variant="light">
-                        Learn more
-                    </Button>{' '}
-                </Col>
-                
+                    <Row className="py-auto justify-content-md-center ">
+                        <Col md="auto" >
+                            <p className={classes.content}>
+                                Wellcome to the Bushido E-shop 
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col className={classes.buttons}>
+                            <Button variant="light">
+                                Shop Now
+                            </Button>{' '}
+                            <Button variant="light">
+                                Learn more
+                            </Button>{' '}
+                        </Col>
+                        
                     
-                
-            </Row>
-            </Container>
+                    </Row>
+                </Container>
                 <video autoPlay loop muted className={classes.video}>
                     <source src={backgroundVideo} type='video/mp4'/>
                 </video>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import NavBar from "../components/NavbarComponent"
 import BackgroundVideo from "../components/BackgroundVideo"
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import FeatureCard from "../components/FeatureCard"
 import classes from "../modules/MainPage.module.css"
 import URL from "../resources/URL.json"
-
+import '../resources/RobotoFont/Roboto-Light.ttf'
+import '../resources/RobotoFont/Roboto-Italic.ttf'
 
 function MainPage()
 {
@@ -14,22 +15,32 @@ function MainPage()
         <React.Fragment>
             <NavBar/>
             <BackgroundVideo />
-            <Container fluid className={classes.featureContainer}>
-                <Row>
-                    <Col md>
-                        {/*  Best Protection Equipment  */}
-                        <FeatureCard imageURL={URL.equipmentURL} feature="Best Protective Equipment" paragraph="We got the best equipment suited for your fighting skills"/>
-                    </Col>
-                    <Col md>
-                        {/* Best Material */}
-                        <FeatureCard imageURL={URL.materialURL} feature="Best Material" paragraph="The best material is used for all the products sold in the Bushido E-Shop"/>
-                    </Col>
-                    <Col md>
-                        {/* Act like a PRO*/}
-                        <FeatureCard/>
-                    </Col>
-                </Row>
-            </Container>
+            <div className={classes.featureContainer}>
+                <Container fluid="md" className=" align-self-center justify-content-md-center " >
+                    <Row className="justify-content-md-center ">
+                        <p className={classes.featureText}>
+                            This is why Bushido Eshop is suited for your martial arts needs.
+                        </p>
+                    </Row>
+                    <Row className="justify-content-md-center ">
+                        <Col className="d-flex justify-content-center ">
+                            {/*  Best Protection Equipment  */}
+                            <FeatureCard imageURL={URL.equipmentURL} feature="Best Protective Equipment" paragraph="We got the best equipment suited for your fighting skills"/>
+                        </Col>
+                        <Col className= "d-flex justify-content-center">
+                            {/* Best Material */}
+                            <FeatureCard imageURL={URL.materialURL} feature="Best Material" paragraph="The best material is used for all the products sold in the Bushido E-Shop"/>
+                        </Col>
+                        <Col className="d-flex justify-content-center">
+                            {/* Act like a PRO*/}
+                            <FeatureCard imageURL={URL.proCoverURL} feature="Become the master" paragraph="Bushido E-Shop will go along with you in your martial arts journey"/>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Button variant="outline-dark" className={classes.featureButton}>Shop now</Button>{''}
+                    </Row>
+                </Container>
+            </div>
         </React.Fragment>
     );
 }
