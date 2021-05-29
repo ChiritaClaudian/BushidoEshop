dabfacc8e12d4a2b01b0180a1e46010174acb1fa
Chirita Claudian <chiritaclaudian@gmail.com>
2021-05-30 02:34:16 +0300

    backend done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,34 +1,80 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-
+import React, {useState} from 'react';
+import {Link, Redirect} from 'react-router-dom'
+import {Modal, Button} from 'react-bootstrap'
+import {useHistory} from 'react-router-dom'
+import { BiAward } from 'react-icons/bi';
 function LoginPage(){
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    let history = useHistory();
+    const handleLogin = async () =>{
+
+        const response = await fetch('http://192.168.0.95:30001/users/' + username);
+        const user = await response.json();
+        if(user == null)
+            setShow(true)
+        else if(user.password !== password)
+            setShow(true)
+        else
+            setIsLoggedIn(true)
+            console.log(show)
+            
+        
+        
+    }
+    if(isLoggedIn){
+        return(
+        <Redirect to='/'/>
+        );
+    }
+    else{
     return(
         <div style={{margin:"10% 35%", display:'flex'}} className="d-flex flex-column">
-            <form>
-                <h3>Login</h3>
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email"/>
-                </div>
+            
+            <h3>Login</h3>
+            <div className="form-group">
+                <label>Username</label>
+                <input type="username" onChange={event => setUsername(event.target.value)} placeholder="Enter username"/>
+            </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password"/>
-                </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" onChange={event => setPassword(event.target.value)} placeholder="Enter password"/>
+            </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
+            <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                 </div>
-                <button style={{paddingLeft:"10%", paddingRight:"10%"}} type="submit" className="btn btn-primary btn-block">Login</button>
-                <p className="forgot-password text-right">
-                    Don't have an account, <Link to="/register">register now!</Link>
-                </p>
-            </form>
+            </div>
+            <button style={{paddingLeft:"10%", paddingRight:"10%"}} className="btn btn-primary btn-block" onClick={handleLogin}>Login</button>
+          
+            
+            <p className="forgot-password text-right">
+                Don't have an account, <Link to="/register">register now!</Link>
+            </p>
+        
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Login error</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>Bad credentials</Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
+    }
 }
 
 export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,35 +1,66 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function RegisterPage(){
+    
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const handleLogin = () => {
+        //ar trb sa schimb pagina si sa setez un cookie
+        //ar trb sa fac post 
+        console.log("hello")
+        var details = {
+            'username': username,
+            'password': password,
+            'mail': email
+            };
+            console.log(username + password + email)
+            var formBody = [];
+            for (var property in details) {
+            var encodedKey = encodeURIComponent(property);
+            var encodedValue = encodeURIComponent(details[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+
+            fetch('http://192.168.0.95:30001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Access-Control-Allow-Origin': 'origin-list'
+            },
+            body: formBody
+            }).then(function(response){
+                console.log(response);
+                return response.json();
+            })
+            .then(function(myJson){
+                console.log(myJson);
+            });
+    }
     return(
         <div style={{margin:"10% 35%", display:'flex'}} className="d-flex flex-column">
-            <form>
-                <h3>Register</h3>
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="firstName" className="form-control" placeholder="Enter first name"/>
-                </div>
+            <h3>Register</h3>
+            <div className="form-group">
+                <label>Username</label>
+                <input type="username" onChange={event => setUsername(event.target.value)} autoComplete="Enter first name"/>
+            </div>
 
                 <div className="form-group">
-                    <label>Last name</label>
-                    <input type="secondName" className="form-control" placeholder="Enter second name"/>
-                </div>
+                <label>Email address</label>
+                <input type="email" onChange={event => setEmail(event.target.value)} autoComplete="Enter email"/>
+            </div>
 
-                
-                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email"/>
-                </div>
+                <div className="form-group">
+                <label>Password</label>
+                <input type="password" onChange={event => setPassword(event.target.value)} autoComplete="Enter password"/>
+            </div>
 
-                 <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password"/>
-                </div>
-
-                <button style={{paddingLeft:"10%", paddingRight:"10%"}} type="submit" className="btn btn-primary btn-block">Signed up</button>
-            </form>
+            <button style={{paddingLeft:"10%", paddingRight:"10%"}} onClick={handleLogin} className="btn btn-primary btn-block">Signed up</button>
+            
         </div>
     );
 }
+
 
 export default RegisterPage;
